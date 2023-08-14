382237d6fee729c778e8df339e893b48f265217d
DaniAguilarGarcia <dagui082@fiu.edu>
2023-08-13 22:00:18 -0400

    new one-budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,8 +2,31 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget: contextBudget } = useContext(AppContext); // Renamed to contextBudget
-    const [localBudget, setLocalBudget] = useState(''); // Changed to localBudget
+    const { budget: contextBudget, expenses, dispatch } = useContext(AppContext); // Added 'expenses'
+    const [localBudget, setLocalBudget] = useState('');
+
+    const totalSpending = expenses.reduce((total, expense) => total + expense.cost, 0);
+
+    const handleBudgetChange = (event) => {
+        setLocalBudget(event.target.value);
+    };
+
+    const handleSaveBudget = () => {
+        if (localBudget !== '') {
+            const enteredBudget = parseFloat(localBudget);
+            if (!isNaN(enteredBudget)) {
+                if (enteredBudget <= 20000) {
+                    if (enteredBudget >= totalSpending) {
+                        dispatch({ type: 'SET_BUDGET', payload: enteredBudget });
+                    } else {
+                        alert('Budget cannot be less than total spending amount');
+                    }
+                } else {
+                    alert('Budget cannot exceed €20,000');
+                }
+            }
+        }
+    };
 
     return (
         <div className='alert alert-secondary'>
@@ -14,11 +37,11 @@ const Budget = () => {
                 id='budget'
                 value={localBudget}
                 style={{ marginLeft: '2rem', size: 10 }}
-                onChange={(event) => setLocalBudget(event.target.value)}>
+                onChange={handleBudgetChange}>
             </input>
+            <button onClick={handleSaveBudget}>Save Budget</button>
         </div>
     );
 };
 
 export default Budget;
-
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -64,6 +64,8 @@ export const AppReducer = (state, action) => {
             return {
                 ...state,
             };
+
+            
         case 'CHG_CURRENCY':
             action.type = "DONE";
             state.currency = action.payload;
